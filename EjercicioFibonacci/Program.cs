1d09a4dd8f5c754e1e8ca1d675fed5e0d42cda2d
Joako119 <joaquindeamelio400@gmail.com>
2023-07-10 23:52:22 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioFibonacci/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador, num1, num2, num3, n;
+            bool salida;
+            contador = 0;
+            num1 = 0;
+            num2 = 1;
+            num3 = 0;
+            salida = true;
+            Console.WriteLine("cuantos numeros de la secuencia de fibonacci se desea conocer");
+            
+            n= int.Parse(Console.ReadLine());
+
+            do
+            {
+                contador++;
+                num3 = num1 + num2;
+                num2 = num1;
+                num1 = num3;
+                Console.WriteLine(num3);
+
+                if (n == contador) { salida = false; } 
+            }
+
+            while (salida);
+
+        }
+    }
+}
